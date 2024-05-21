4e9b1a975d538f75e4971f1998eb5f53e005f576
Mikey Espi <espihawk@outlook.com>
2024-05-21 18:56:19 -0400

    Added vue to goal.js file

--- goal.js
+++ goal.js
@@ -1,1 +1,27 @@
 Vue.createApp();
+const { createApp } = Vue
+
+        const app = createApp({
+            
+            data() {
+                return {
+                  pageViews: 0,
+                };
+              },
+              
+              mounted() {
+                // Retrieve count from localStorage
+                const storedCount = localStorage.getItem('pageCount');
+                if (storedCount) {
+                  this.pageCount = parseInt(storedCount);
+                  pageViews += 1;
+                }
+              },
+              beforeDestroy() {
+                // Save count to localStorage
+                localStorage.setItem('pageCount', this.pageCount.toString());
+              },
+            })
+        app.mount('#app')
+
+        
